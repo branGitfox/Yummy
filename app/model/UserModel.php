0a0dc39fbb8d7dbef8772976578cf8e82782374f
BranGitfox <vixfgit@gmail.com>
2024-10-28 15:49:02 +0300

    UserModel:method for checking exist email created

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -19,5 +19,31 @@ class UserModel extends Database {
      * @return []|null
      */
 
-     
+     public function CreateUser(String $username,String $email, String $password, String $adress, String $tel, String $profil ): void{
+         $query = parent::getPdo()->prepare('INSERT INTO users(`username`,`email`,`password`,`adress`,`tel`, `profil`) VALUES (?,?,?,?,?,?)');
+         $query->execute([$username,$email, $password, $adress, $tel, $profil]);
+     }
+
+    /**
+     * Va tester si un email existe deja dans la table users
+     * @return bool
+     */
+
+     private function CheckExistEmail(String $email):bool{
+        $query = parent::getpdo()->prepare('SELECT email FROM users WHERE email = ?');
+        $query->execute([$email]);
+        if($query->rowCount() < 0){
+            return true;
+        }else{
+            return false;
+        }
+     }
+      
+     /**
+      * @return bool|String
+      */
+     public function ConnecUser() {
+
+     }
+
 }
