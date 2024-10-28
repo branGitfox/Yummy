e37fc9c278e151ee2b368fb726f01212485697f4
BranGitfox <vixfgit@gmail.com>
2024-10-28 14:41:04 +0300

    separating navbar

--- index.php
+++ index.php
@@ -1,53 +1,34 @@
 <?php $title = 'home';
 require './app/view/header/header.php'; ?>
-<div class="container">
+<div id="container" class="container" style="height:100vh; overflow-y:scroll">
 
-    <nav class="navbar navbar-expand-lg ">
-        <div class="container-fluid">
-            <a class="navbar-brand text-warning"  href="#" style="font-family: Arial, Helvetica, sans-serif;">YUMMY</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">A propos de nous</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Rechercher</button>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <!-- navbar -->
+    <?php require './app/view/nav/nav.php' ?>
+    <!-- image pizza -->
     <div class="container" style="height: 500px; overflow:hidden;">
         <img src="./assets/img/pizza.jpg" class="img-fluid" alt="...">
     </div>
+    <!-- Image produit -->
         <div class="container d-flex flex-wrap justify-content-center gap-5 p-2 mt-3">
             <img src="./assets/img/pizza.jpg" class="shadow"  alt="..." style="width:300px;height:200px;opacity:.9"> 
             <img src="./assets/img/pizza.jpg"  class="shadow" alt="..." style="width:300px;height:200px;opacity:.9">
             <img src="./assets/img/pizza.jpg" class="shadow"  alt="..." style="width:300px;height:200px;opacity:.9">
         </div>
+        <!-- menu et filtres -->
     <div class="container mt-1 bg-light p-3" >
         <h1 class="text-center" style="font-weight:bold; font-family:'Courier New', Courier, monospace">Notre Menu</h1>
         <div class="container d-flex flex-wrap gap-5 justify-content-center mt-5">
                 <a href="" class="btn btn-outline-warning" style="border-radius:20px;">Pizzas</a>
                 <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Soupes</a>
                 <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Riz</a>
-                <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Tacoss</a>
-                <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Samboss</a>
+                <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Tacos</a>
+                <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Sambos</a>
                 <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Gratins</a>
                 <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Boissons</a>
                 <a href="" class="btn btn-outline-secondary" style="border-radius:20px;">Glaces</a>
         </div>
     </div>
+
     
 
 </div>
