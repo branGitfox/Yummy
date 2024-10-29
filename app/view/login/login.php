b28cb06ef1d46723eec19ee03e228347a8871c05
BranGitfox <vixfgit@gmail.com>
2024-10-29 19:33:59 +0300

    login:login form done

--- app/view/login/login.php
+++ app/view/login/login.php
@@ -1,6 +1,27 @@
 <?php $title='Yummy | Login'; $dir='../../..';require '../header/header.php'; ?>   
 
-<h1 class='text-center'>Hello world</h1>
+<div class="container mt-5 p-3 shadow rounded ">
+    <h1 class='text-center'>YUMMY😋</h1>
+    <p class='text-center'>Vous avez faim? Commandez chez nous !</p>
+    <h3 class='text-center'>Se connecter.</h3>
+    <div class="form-group">
+        <div class='mb-2'>
+            <label for="email">Email</label>
+            <input type="email" class="form-control">
+        </div>
+        <div class='mb-2'>
+            <label for="email">Mot de passe</label>
+            <input type="password" class="form-control">
+        </div>
+        <div class='mt-2'>
+            
+            <input type="submit"  class="form-control btn btn-warning" value='Se connecter'>
+    </div>
+        <div class="container mt-3 text-center">
+            <a  href="">Pas de compte? s'inscrire</a>
+        </div>
+    </div>
+</div>
 
 <?php require '../footer/footer.php'; ?>
 
