c1b635b02f963056e30a0c0e809c5ff634a8108a
BranGitfox <vixfgit@gmail.com>
2024-10-31 00:20:59 +0300

    index: search bar working

--- app/model/ProductModel.php
+++ app/model/ProductModel.php
@@ -5,14 +5,14 @@
 class ProductModel extends Database {
     public function getProduct() {
         if(isset($_GET['q'])){
-            $query = parent::getPdo()->prepare("SELECT * FROM product WHERE prodname LIKE '%{?}%'");
-            $query->execute([$_GET['q']]);
+            $query = parent::getPdo()->prepare("SELECT * FROM product WHERE prodname LIKE '%{$_GET['q']}%'");
+            $query->execute();
         }elseif(isset($_GET['f'])){
             $query = parent::getPdo()->prepare("SELECT * FROM product WHERE category_id = ?");
             $query->execute([$_GET['f']]);
         }elseif(isset($_GET['q']) && isset($_GET['f'])){
-            $query = parent::getPdo()->prepare("SELECT * FROM product WHERE prodname LIKE '%{?}%' AND category_id = ?");
-            $query->execute([$_GET['q'], $_GET['f']]);
+            $query = parent::getPdo()->prepare("SELECT * FROM product WHERE prodname LIKE '%{$_GET['q']}%' AND category_id = ?");
+            $query->execute([$_GET['f']]);
         }else{
             $query = parent::getPdo()->prepare("SELECT * FROM product");
             $query->execute();
